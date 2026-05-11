8b42626cec6628f2c137314d0fc0c3e7ac981a1c
shotek123 <odouda26@gmail.com>
2026-05-11 11:59:10 +0200

    Initial commit - struktura projektu a prvni trida NetworkDevice

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+class NetworkDevice {
+    id;
+    name;
+    price;
+    powerConsumption;
+}
---
+++ script.ts
@@ -0,0 +1,7 @@
+abstract class NetworkDevice {
+    private id: string;
+    private name: string;
+    private price: number;
+    private powerConsumption: number;
+}
+
